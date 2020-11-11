fb7dae72e44a68fda5daba69cae857f30e3cac53
Зинченко Василий <zinchenkovasiliyserg@gmail.com>
2020-11-11 15:48:54 +0500

    Добавил выбор фото и аватара

--- js/form.js
+++ js/form.js
@@ -110,15 +110,6 @@ const successHandler = function (data) {
   window.pin.generatePinTemplate(data);
 };
 
-
-const checkPopup = function () {
-  const popup = MAP.querySelector(`.popup`);
-  if (popup) {
-    MAP.removeChild(popup);
-  }
-};
-
-
 let filteredState = {
   type: `any`,
   price: `any`,
@@ -126,121 +117,31 @@ let filteredState = {
   guests: `any`
 };
 
-const priceRange = {
-  low: {
-    min: 0,
-    max: 10000
-  },
-  middle: {
-    min: 10000,
-    max: 50000
-  },
-  high: {
-    min: 50000,
-    max: 1000000
-  }
-};
-
-const DEFAULT_TYPE = `any`;
-
-const filteredByType = function (arr, type) {
-  return type !== DEFAULT_TYPE ? arr.filter((item) => item.offer.type === type) : arr;
-};
-
-const filteredByRooms = function (arr, type) {
-  return type !== DEFAULT_TYPE ? arr.filter((item) => parseInt(item.offer.rooms, 10) === parseInt(type, 10)) : arr;
-
-};
-
-const filteredByGuests = function (arr, type) {
-  return type !== DEFAULT_TYPE ? arr.filter((item) => parseInt(item.offer.guests, 10) === parseInt(type, 10)) : arr;
-};
-
-const filteredByPrice = function (arr, type) {
-  return type !== DEFAULT_TYPE ? arr.filter((item) => item.offer.price >= priceRange[type].min && item.offer.price <= priceRange[type].max) : arr;
-
-};
-
-const findFeature = function (features) {
-  let match = 0;
-  let flag = false;
-  features.forEach((feature)=>{
-    if (comparedFeatures.indexOf(feature) !== -1) {
-      match += 1;
-    }
-  });
-  if (match === comparedFeatures.length) {
-    flag = true;
-  }
-  return flag;
-};
-
-const filteredByFeatures = function (arr) {
-  return arr.filter((item)=>findFeature(item.offer.features));
-};
-
-
-const filteredHandler = function () {
-  checkPopup();
-  let filtered = [];
-  filtered = filteredByType(adverts, filteredState.type);
-  filtered = filteredByPrice(filtered, filteredState.price);
-  filtered = filteredByRooms(filtered, filteredState.rooms);
-  filtered = filteredByGuests(filtered, filteredState.guests);
-  filtered = filteredByFeatures(filtered);
-  window.pin.generatePinTemplate(filtered);
-};
 
 HOUSING_TYPE.addEventListener(`change`, function (evt) {
   filteredState.type = evt.target.value;
-  window.debounce(filteredHandler());
+  window.debounce(window.filter.byType(adverts, filteredState));
 });
 
 HOUSING_PRICE.addEventListener(`change`, function (evt) {
   filteredState.price = evt.target.value;
-  window.debounce(filteredHandler());
+  window.debounce(window.filter.byType(adverts, filteredState));
 });
 
 HOUSING_ROOMS.addEventListener(`change`, function (evt) {
   filteredState.rooms = evt.target.value;
-  window.debounce(filteredHandler());
+  window.debounce(window.filter.byType(adverts, filteredState));
 });
 
 HOUSING_QUESTS.addEventListener(`change`, function (evt) {
   filteredState.guests = evt.target.value;
-  window.debounce(filteredHandler());
+  window.debounce(window.filter.byType(adverts, filteredState));
 });
-
-const FEATURES_MAP = {
-  "Wi-Fi": `wifi`,
-  "Посудомоечная машина": `dishwasher`,
-  "Парковка": `parking`,
-  "Стиральная машина": `washer`,
-  "Лифт": `elevator`,
-  "Кондиционер": `conditioner`
-};
-
-let comparedFeatures = [];
-
-const getMatch = function (arr, type) {
-  let match = ``;
-  match = arr.includes(type);
-  if (!match) {
-    arr.push(type);
-  } else {
-    let index = comparedFeatures.indexOf(type);
-    arr.splice(index, 1);
-  }
-};
-
-const getCompareFeatures = function (feature) {
-  getMatch(comparedFeatures, FEATURES_MAP[feature]);
-};
 
 FEATURES.forEach((item) =>{
   item.addEventListener(`click`, function (evt) {
-    getCompareFeatures(evt.target.textContent);
-    window.debounce(filteredHandler());
+    window.filter.byFeature(evt.target.textContent);
+    window.debounce(window.filter.byType(adverts, filteredState));
   });
 });
 
